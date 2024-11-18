8f6e3d36a14922533d231351b5dfd2de7642572e
Jacob Terpe Woods <jacobterpewoods@gmail.com>
2024-11-18 14:43:22 +0100

    fix: auth mock in test

--- routes/voteRoutes.js
+++ routes/voteRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { vote } from '../controllers/vote.js';
-import auth from '../middleware/auth.js';
+import { auth } from '../middleware/auth.js';
 
 const router = express.Router();
 
