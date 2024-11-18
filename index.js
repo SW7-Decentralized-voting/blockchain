dae23c1e7c452d9468dac09b43582ab4f13cb1c0
Jacob Terpe Woods <jacobterpewoods@gmail.com>
2024-11-18 14:06:17 +0100

    fix: db connection when starting server

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import router from './routes/index.js';
 import { PORT } from './config/config.js';
+import { connectToDb } from './db/index.js';
 
 const app = express();
 app.use(express.json());
 
 app.use('/', router);
+
+connectToDb(process.env.MONGO_URI);
 
 // Start the server
 app.listen(PORT, () => {
