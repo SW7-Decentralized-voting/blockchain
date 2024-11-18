c2c0537427691f6c291cfcf2a9f62835e6acf483
Jacob Terpe Woods <jacobterpewoods@gmail.com>
2024-11-18 14:31:56 +0100

    fix: cors settings + auth for vote route

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import router from './routes/index.js';
 import { PORT } from './config/config.js';
 import { connectToDb } from './db/index.js';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
 
+app.use(cors());
 app.use('/', router);
 
 connectToDb(process.env.MONGO_URI);
